478b7c403d852f2ceb90d8fd5db0640f25e13247
Jonathan H. Wage <jonwage@gmail.com>
2015-03-28 18:02:53 -0500

    Updates to doctrine developvm.

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,11 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "ubuntu/trusty64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, host: 4567, guest: 80
   config.ssh.forward_agent = true
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 2
+  end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
 apt-get install -y software-properties-common
 apt-get install -y python-software-properties
 
@@ -9,7 +12,8 @@ apt-get update
 
 apt-get install -y openjdk-6-jre
 apt-get install -y openjdk-7-jdk
-apt-get install -y php5 php5-curl
+apt-get install -y php5 php5-curl php5-mongo
+apt-get install -y mongodb
 apt-get install -y vim
 apt-get install -y curl
 apt-get install -y git
@@ -27,19 +31,20 @@ pip install tinkerer
 pip install cssselect
 pip install lxml
 
-rm -rf /var/www
-ln -s /vagrant/data/doctrine-website-sphinx/site/blog/html /var/www
+wget https://phar.phpunit.de/phpunit.phar
+mv phpunit.phar /usr/bin/phpunit
+chmod +x /usr/bin/phpunit
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
 
 if [ ! -d "/vagrant/data" ]; then
     mkdir /vagrant/data
 fi
 
+rm -rf /var/www
+ln -s /vagrant/data/doctrine-website-sphinx/site/blog/html /var/www
+
 cd /vagrant/data
 git clone git@github.com:doctrine/doctrine-sphinx-theme.git
 git clone git@github.com:doctrine/doctrine-website-sphinx.git
-./bin/generate-github-token.sh
-ant install-sphinx
-ant build
-
-export VISUAL=vim
-export EDITOR="$VISUAL"
